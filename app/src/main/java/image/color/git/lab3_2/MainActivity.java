b371548606f293b66962df4ccccbb6754e4a713f
DubstepKnight <nursmaster@gmail.com>
2020-03-31 19:18:52 +0300

    webIntent has been made

--- app/src/main/java/image/color/git/lab3_2/MainActivity.java
+++ app/src/main/java/image/color/git/lab3_2/MainActivity.java
@@ -15,9 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openWebsite(View view) {
+        Uri oamkUrl = Uri.parse("http://www.oamk.fi");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, oamkUrl);
+        startActivity(webIntent);
+    }
+
     public void callMe(View view) {
         Log.d("callMe", "the button was clicked");
-        Uri number = Uri.parse("tel: +358 40 372 3106");
+        Uri number = Uri.parse("tel: +358 20 611 0200");
         Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
         startActivity(callIntent);
     }
