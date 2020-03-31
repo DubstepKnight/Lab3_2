0537db01c94695e0f1b02e7694cdff5a786ac1a3
DubstepKnight <nursmaster@gmail.com>
2020-03-31 18:57:08 +0300

    call intent has been made

--- app/src/main/java/image/color/git/lab3_2/MainActivity.java
+++ app/src/main/java/image/color/git/lab3_2/MainActivity.java
@@ -2,7 +2,11 @@ package image.color.git.lab3_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void callMe(View view) {
+        Log.d("callMe", "the button was clicked");
+        Uri number = Uri.parse("tel: +358 40 372 3106");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+        startActivity(callIntent);
+    }
+
 }
