f073540277394e88e8b6ecef0fd626488ad1e2de
DubstepKnight <nursmaster@gmail.com>
2020-04-04 12:18:28 +0300

    Lab3_2 is done

--- app/src/main/java/image/color/git/lab3_2/MainActivity.java
+++ app/src/main/java/image/color/git/lab3_2/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openWebsite(View view) {
+    public void openOamk(View view) {
         Uri oamkUrl = Uri.parse("http://www.oamk.fi");
         Intent webIntent = new Intent(Intent.ACTION_VIEW, oamkUrl);
         startActivity(webIntent);
@@ -27,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
         Uri number = Uri.parse("tel: +358 20 611 0200");
         Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
         startActivity(callIntent);
+    }
+
+//This function fully works, you just need to add 'http://www' in the begginning of the URL
+    public void openWebsite(View view) {
+        Log.d("click", "the button was clicked");
+        EditText uriToOpen = findViewById(R.id.uriToOpen);
+        Uri typedUri =  Uri.parse(uriToOpen.getText().toString());
+        Log.d("click", typedUri + "");
+        Intent openPageIntent = new Intent(Intent.ACTION_VIEW, typedUri);
+        startActivity(openPageIntent);
     }
 
 }
